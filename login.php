b54209bda377f6ffab8c3fe640f3e637fb4a222f
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-03-03 23:55:47 +0545

    modified in  session

--- login.php
+++ login.php
@@ -9,13 +9,13 @@ if (isset($_POST['submit'])) {
  //OR it Retrieve values from the submitted form
     $username = $_POST['username'];
     $password = $_POST['pass'];
+    
 }
-
 // SQL query to check if the provided username and password exist in the 'admin' table
-$checkadmin = "SELECT * from admin WHERE user_name='$username' AND user_pass=$password";
+$checkadmin = "SELECT * from admin WHERE (user_name='$username' OR email='$username') AND user_pass='$password'";
 
 // SQL query to check if the provided username and password exist in the 'register_employee' table
-$checkemployee = "SELECT * from register_employee WHERE user_name='$username' AND user_pass='$password'";
+$checkemployee = "SELECT * from register_employee WHERE (user_name='$username' OR email='$username') AND user_pass='$password'";
 
 // Execute the SQL query
 $resultadmin = $conn->query($checkadmin);
@@ -24,18 +24,24 @@ $resultemployee = $conn->query($checkemployee);
 // Check if the number of rows returned by the query is greater than zero
  // Check if the username and password match in the admin table
 if ($resultadmin->num_rows > 0) {
-  
-    // Store user in the session variable i.e $username.
-    $_SESSION['user'] = $username;
+
+// fetch the data from the admin table
+  $database_fetch = $resultadmin->fetch_assoc();
+
+ // $_SESSION['user'] is a session variable named "user" which holds value of 'user_name' from database
+  $_SESSION['user'] = $database_fetch ['user_name']; // this 'user_name' is database field name
 
     // If rows are returned, redirect to the home.php page
     header("Location: home.php");
     exit();
 } elseif ($resultemployee->num_rows > 0){
 
- // Store user in the session variable
- $_SESSION['user'] = $username;
+// fetch the data from the admin table
+$database_fetch = $resultemployee->fetch_assoc();
 
+// $_SESSION['user'] is a session variable named "user" which holds value of 'user_name' from database
+ $_SESSION['user'] = $database_fetch ['user_name']; // this 'user_name' is database field name
+ 
  // Redirect to the dashboard.php page
  header("Location: dashboard.php");
  exit();
