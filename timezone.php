9384f33c023e29fab90c48e40cb2c296c32bcd37
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-03-13 22:14:26 +0545

    commented here

--- timezone.php
+++ timezone.php
@@ -1,4 +1,5 @@
 <?php
 	$timezone = 'Asia/Kathmandu';
-	date_default_timezone_set($timezone);
+//date_default_timezone_set() function is used to set the default timezone & variable $timezone is passed as the argument to the function.
+	date_default_timezone_set($timezone); 
 ?>
