28420c77da268fccfc46d3363fea73ac0f7b3308
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-03-21 17:54:30 +0545

    deleted attendance history

---
+++ delete_attendance.php
@@ -0,0 +1,25 @@
+<?php
+include('db_connect.php');
+
+// Check if the 'id' parameter is set in the URL
+if (isset($_GET['id'])) {
+    // Retrieve the value of the 'id' parameter
+    $employeeID = $_GET['id'];
+
+//query to delete the record from the table & employee_id is column name of database & u can give randomvariable name instead '$employeeID'.
+$deletequery = "DELETE FROM attendance where employee_id = '$employeeID'";
+$result = mysqli_query($conn, $deletequery);
+if ($result) {
+    header('location:view_attendance.php');
+}
+else {
+    die("Delete failed: " . mysqli_error($conn));
+}
+}
+?>
+
+
+
+
+
+
