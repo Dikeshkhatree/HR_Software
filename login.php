a4d1408091562b1164e9381b1b6f2315f68e0ea5
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-04-14 15:30:05 +0545

    just for insert attednance

--- login.php
+++ login.php
@@ -8,8 +8,7 @@ if (isset($_POST['submit'])) {
 //if form is submitted, it retrieves the values from submitted form using $_POST.
  //OR it Retrieve values from the submitted form
     $username = $_POST['username'];
-    $password = $_POST['pass'];
-    
+    $password = $_POST['pass'];   
 }
 // SQL query to check if the provided username and password exist in the 'admin' table
 $checkadmin = "SELECT * from admin WHERE (user_name='$username' OR email='$username') AND user_pass='$password'";
