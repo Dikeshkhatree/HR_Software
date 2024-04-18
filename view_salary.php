6d24528d8a4f798b6f7118b4dbe024c9b29ca76c
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-04-18 23:43:14 +0545

    view calculated salary in table

---
+++ view_salary.php
@@ -0,0 +1,74 @@
+<?php
+include('home.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>salary Details</title>
+   <link rel="stylesheet" href="css/view_salary.css"/>
+   
+</head>
+<body>
+   <div class="main-container">
+   <div class="table-container">
+      <p class="TableInfo">Salary Details</p>
+      <div class="table-div">
+    
+    <table>
+        <thead>
+
+              <th>Date</th>
+                <th>Employee ID</th>
+                <th>Username</th>
+                <th>Role</th>
+                <th>Total Salary</th>
+                <th>Action</th>
+           
+        </thead>
+        <tbody>
+            <?php
+            // Include the file with the database connection
+            include("db_connect.php");
+
+       // SQL query to retrieve data from the 'employee_detail' table
+            $selectQuery = "SELECT * FROM payroll ORDER BY Date DESC";
+
+            // Execute the SQL query
+            $result = $conn->query($selectQuery);
+
+          // Iterate through each row in the result set & loop continues until there are no more rows left 
+                while ($row = $result->fetch_assoc()) {
+          // Extract data from the database and assign it to variables given below.
+                    $date = $row['date'];
+                    $employeeID = $row['employee_id'];
+                    $username = $row['username'];
+                    $role = $row['role'];
+                    $salary = $row['salary'];
+                                 
+               // output of HTML table row
+                    echo "<tr>
+                    <td>".$date."</td>
+                    <td>".$employeeID."</td>
+                    <td>".$username."</td>      
+                    <td>".$role."</td>
+                    <td>".$salary."</td>
+                                                       
+                    <td class='action-column'>
+                  
+                    <!-- $employeeID is a PHP variable holding the value of the id parameter. -->
+  
+                    <a class='edit' href='update_salary.php?id=$employeeID'>Update</a>  
+                  </td>
+                  </tr>";
+                }
+            
+            ?>
+        </tbody>
+    </table>
+    </div>
+   </div>
+   </div>
+</body>
+</html>
