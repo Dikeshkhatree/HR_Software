adda6515d43a6c79dd8da9589e767160eaf0e0b9
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-05-03 18:57:07 +0545

    regex added to accept string

--- department.php
+++ department.php
@@ -3,10 +3,36 @@
 session_start();
 // Include the file with the database connection
 include("db_connect.php");
+
+// Function to check if a string contains only alphabetic characters
+function isValidDepartmentName($department) {
+    return preg_match('/^[A-Za-z\s]+$/', $department); // used regex
+}
+
 // Check if the form is submitted
-if (isset($_POST['department_name'])) {
+if (isset($_POST["submit"])) {
     // Retrieve department name from the form
     $department = $_POST['department_name'];
+
+  // Check if department name is provided or check for empty
+  if(empty($department)) { 
+    // Set error message for missing department name
+    $_SESSION['error_message'] = "Please fill out the department name field.";
+
+    // Redirect to the frontend page
+    header("Location: add_department.php");
+    exit();
+}
+
+    // Check if the department name is valid
+    if (!isValidDepartmentName($department)) {
+        // Set error message for invalid department name
+        $_SESSION['error_message'] = "Invalid department name. Please enter only alphabetic characters.";
+
+        // Redirect to the frontend page
+        header("Location: add_department.php");
+        exit();
+    }
 
     // Construct the SQL query to insert the department name into the database
     $insertQuery = "INSERT INTO department (department) VALUES ('$department')";
@@ -15,14 +41,14 @@ if (isset($_POST['department_name'])) {
     if ($conn->query($insertQuery) === TRUE) {
         // Set success message
         $_SESSION['success_message'] = "Department added successfully !";
-        
+
         // Redirect to the frontend page
         header("Location: add_department.php");
         exit();
     } else {
         // If insertion fails, display an error message
         $_SESSION['error_message'] = "Error: " . $insertQuery . "<br>" . $conn->error;
-        
+
         // Redirect to the frontend page
         header("Location: add_department.php");
         exit();
