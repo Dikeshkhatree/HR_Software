cc09aa4e44dcd3216df64abb24d84bf59476bd83
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-06-02 22:13:53 +0545

    add img

---
+++ img.php
@@ -0,0 +1,22 @@
+<?php
+include('dashboard.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image</title>
+    <style>
+       .resized-image {
+        width: 1275px;
+    height: 630px;
+    margin-left: 241px;
+    margin-top: 61px;
+       }
+    </style>
+</head>
+<body>
+    <img src="images/employee.jpeg" alt="hey" class="resized-image" />
+</body>
+</html>
