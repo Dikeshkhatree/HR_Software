19ed5889636de90afd7bf4892e5b5e0d4bfe80c9
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-06-02 22:03:09 +0545

    delete detail

--- deleteemp_detail.php
+++ deleteemp_detail.php
@@ -7,7 +7,7 @@ if (isset($_GET['id'])) {
     $employeeID = $_GET['id'];
 
 //query to delete the record from the table & employee_id is column name of database & u can give randomvariable name instead '$employeeID'.
-$deletequery = "DELETE FROM add_detail where employee_id = '$employeeID'";
+$deletequery = "DELETE FROM employee where employee_id = $employeeID";
 $result = mysqli_query($conn, $deletequery);
 if ($result) {
     header('location:viewdetail.php');
