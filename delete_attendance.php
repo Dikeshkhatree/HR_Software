0c54960a30de6d292783350d55777a9f5d41755c
Dikeshkhatree <dikeshkhatree@gmail.com>
2024-06-02 21:56:06 +0545

    delete att

--- delete_attendance.php
+++ delete_attendance.php
@@ -5,9 +5,10 @@ include('db_connect.php');
 if (isset($_GET['id'])) {
     // Retrieve the value of the 'id' parameter
     $employeeID = $_GET['id'];
+    $Date = $_GET['date'];
 
 //query to delete the record from the table & employee_id is column name of database & u can give randomvariable name instead '$employeeID'.
-$deletequery = "DELETE FROM attendance where employee_id = '$employeeID'";
+$deletequery = "DELETE FROM attendance where employee_id = '$employeeID' AND date = '$Date'";
 $result = mysqli_query($conn, $deletequery);
 if ($result) {
     header('location:view_attendance.php');
